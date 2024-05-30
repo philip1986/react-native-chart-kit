94f5d8d51dfee0f6accee1cd009e0e1d0ac6d2ef
Philip <>
2024-05-30 16:29:17 +0200

    addPieChart animation

--- src/PieChart.tsx
+++ src/PieChart.tsx
@@ -1,6 +1,6 @@
 import Pie from "paths-js/pie";
-import React from "react";
-import { View, ViewStyle } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Animated, Easing, View, ViewStyle } from "react-native";
 import { G, Path, Rect, Svg, Text } from "react-native-svg";
 
 import AbstractChart, { AbstractChartProps } from "./AbstractChart";
@@ -17,6 +17,7 @@ export interface PieChartProps extends AbstractChartProps {
   hasLegend?: boolean;
   style?: Partial<ViewStyle>;
   avoidFalseZero?: boolean;
+  animationDuration?: number;
 }
 
 type PieChartState = {};
@@ -28,82 +29,10 @@ class PieChart extends AbstractChart<PieChartProps, PieChartState> {
       backgroundColor,
       absolute = false,
       hasLegend = true,
-      avoidFalseZero = false
+      avoidFalseZero = false,
     } = this.props;
 
     const { borderRadius = 0 } = style;
-
-    const chart = Pie({
-      center: this.props.center || [0, 0],
-      r: 0,
-      R: this.props.height / 2.5,
-      data: this.props.data,
-      accessor: x => {
-        return x[this.props.accessor];
-      }
-    });
-
-    const total = this.props.data.reduce((sum, item) => {
-      return sum + item[this.props.accessor];
-    }, 0);
-
-    const slices = chart.curves.map((c, i) => {
-      let value: string;
-
-      if (absolute) {
-        value = c.item[this.props.accessor];
-      } else {
-        if (total === 0) {
-          value = 0 + "%";
-        } else {
-          const percentage = Math.round(
-            (100 / total) * c.item[this.props.accessor]
-          );
-          value = Math.round((100 / total) * c.item[this.props.accessor]) + "%";
-          if (avoidFalseZero && percentage === 0) {
-            value = "<1%";
-          } else {
-            value = percentage + "%";
-          }
-        }
-      }
-
-      return (
-        <G key={Math.random()}>
-          <Path d={c.sector.path.print()} fill={c.item.color} />
-          {hasLegend ? (
-            <Rect
-              width="16px"
-              height="16px"
-              fill={c.item.color}
-              rx={8}
-              ry={8}
-              x={this.props.width / 2.5 - 24}
-              y={
-                -(this.props.height / 2.5) +
-                ((this.props.height * 0.8) / this.props.data.length) * i +
-                12
-              }
-            />
-          ) : null}
-          {hasLegend ? (
-            <Text
-              fill={c.item.legendFontColor}
-              fontSize={c.item.legendFontSize}
-              fontFamily={c.item.legendFontFamily}
-              x={this.props.width / 2.5}
-              y={
-                -(this.props.height / 2.5) +
-                ((this.props.height * 0.8) / this.props.data.length) * i +
-                12 * 2
-              }
-            >
-              {`${value} ${c.item.name}`}
-            </Text>
-          ) : null}
-        </G>
-      );
-    });
 
     return (
       <View
@@ -111,7 +40,7 @@ class PieChart extends AbstractChart<PieChartProps, PieChartState> {
           width: this.props.width,
           height: this.props.height,
           padding: 0,
-          ...style
+          ...style,
         }}
       >
         <Svg width={this.props.width} height={this.props.height}>
@@ -119,7 +48,7 @@ class PieChart extends AbstractChart<PieChartProps, PieChartState> {
             {this.renderDefs({
               width: this.props.height,
               height: this.props.height,
-              ...this.props.chartConfig
+              ...this.props.chartConfig,
             })}
           </G>
           <Rect
@@ -136,7 +65,17 @@ class PieChart extends AbstractChart<PieChartProps, PieChartState> {
             }
             y={this.props.height / 2}
           >
-            {slices}
+            <Slices
+              data={this.props.data}
+              accessor={this.props.accessor}
+              width={this.props.width}
+              height={this.props.height}
+              center={this.props.center}
+              absolute={absolute}
+              hasLegend={hasLegend}
+              avoidFalseZero={avoidFalseZero}
+              animationDuration={this.props.animationDuration}
+            />
           </G>
         </Svg>
       </View>
@@ -145,3 +84,233 @@ class PieChart extends AbstractChart<PieChartProps, PieChartState> {
 }
 
 export default PieChart;
+
+interface SlicesProps {
+  data: Array<any>;
+  accessor: string;
+  width: number;
+  height: number;
+  center: Array<number>;
+  absolute: boolean;
+  hasLegend: boolean;
+  avoidFalseZero: boolean;
+  animationDuration?: number;
+}
+
+function Slices({
+  data,
+  accessor,
+  width,
+  height,
+  center,
+  absolute,
+  hasLegend,
+  avoidFalseZero,
+  animationDuration,
+}: SlicesProps) {
+  const total = data.reduce((sum, item) => {
+    return sum + item[accessor];
+  }, 0);
+
+  const chart = Pie({
+    center: center || [0, 0],
+    r: 0,
+    R: height / 2.5,
+    data: data,
+    accessor: (x) => {
+      return x[accessor];
+    },
+  });
+
+  return chart.curves.map((c, i) => {
+    let value: string;
+
+    if (absolute) {
+      value = c.item[accessor];
+    } else {
+      if (total === 0) {
+        value = 0 + "%";
+      } else {
+        const percentage = Math.round((100 / total) * c.item[accessor]);
+        value = Math.round((100 / total) * c.item[accessor]) + "%";
+        if (avoidFalseZero && percentage === 0) {
+          value = "<1%";
+        } else {
+          value = percentage + "%";
+        }
+      }
+    }
+
+    return (
+      <G key={Math.random()}>
+        <Slice
+          index={i}
+          data={data}
+          center={center}
+          height={height}
+          accessor={accessor}
+          total={total}
+          animationDuration={animationDuration}
+        />
+        {hasLegend && (
+          <LegendItem
+            width={width}
+            height={height}
+            index={i}
+            text={c.item.name}
+            color={c.item.color}
+            value={value}
+            legendFontColor={c.item.legendFontColor}
+            legendFontFamily={c.item.legendFontFamily}
+            legendFontSize={c.item.legendFontSize}
+            length={chart.curves.length}
+            animationDuration={animationDuration}
+          />
+        )}
+      </G>
+    );
+  });
+}
+
+interface LegendProps {
+  width: number;
+  height: number;
+  text: string;
+  value: string;
+  color: string;
+  legendFontColor: string;
+  legendFontSize: number;
+  legendFontFamily: string;
+  length: number;
+  index: number;
+  animationDuration?: number;
+}
+
+function LegendItem({
+  width,
+  height,
+  text,
+  color,
+  value,
+  legendFontColor,
+  legendFontSize,
+  legendFontFamily,
+  length,
+  index,
+  animationDuration,
+}: LegendProps) {
+  const [opacity, setOpacity] = useState(animationDuration ? 0 : 1);
+
+  useEffect(() => {
+    if (!animationDuration) return;
+    const animationValue = new Animated.Value(0);
+
+    Animated.timing(animationValue, {
+      toValue: 1,
+      duration: animationDuration * 3,
+      useNativeDriver: false,
+      easing: Easing.inOut(Easing.ease),
+    }).start();
+
+    const subscription = animationValue.addListener(({ value }) => {
+      setOpacity(value);
+    });
+
+    return () => {
+      animationValue.removeListener(subscription);
+    };
+  }, []);
+
+  return (
+    <>
+      <Rect
+        width="16px"
+        height="16px"
+        fill={color}
+        opacity={opacity}
+        rx={8}
+        ry={8}
+        x={width / 2.5 - 24}
+        y={-(height / 2.5) + ((height * 0.8) / length) * index + 12}
+      />
+
+      <Text
+        fill={legendFontColor}
+        fontSize={legendFontSize}
+        fontFamily={legendFontFamily}
+        opacity={opacity}
+        x={width / 2.5}
+        y={-(height / 2.5) + ((height * 0.8) / length) * index + 12 * 2}
+      >
+        {`${value} ${text}`}
+      </Text>
+    </>
+  );
+}
+
+interface SliceProps {
+  index: number;
+  data: Array<any>;
+  center: Array<number>;
+  height: number;
+  accessor: string;
+  total: number;
+  animationDuration?: number;
+}
+
+function Slice({
+  index,
+  data,
+  center,
+  height,
+  accessor,
+  total,
+  animationDuration,
+}: SliceProps) {
+  const [chart, setChart] = useState(
+    Pie({
+      center: center || [0, 0],
+      r: 0,
+      R: height / 2.5,
+      data,
+      accessor: (x) => x[accessor],
+    })
+  );
+
+  useEffect(() => {
+    if (!animationDuration) return;
+    const animationValue = new Animated.Value(1);
+
+    Animated.timing(animationValue, {
+      toValue: 0,
+      duration: animationDuration,
+      useNativeDriver: false,
+      easing: Easing.inOut(Easing.ease),
+    }).start();
+
+    const subscription = animationValue.addListener(({ value }) => {
+      setChart(
+        Pie({
+          center: center || [0, 0],
+          r: 0,
+          R: height / 2.5,
+          data: [...data, { [accessor]: total * 10 * value }],
+          accessor: (x) => x[accessor],
+        })
+      );
+    });
+
+    return () => {
+      animationValue.removeListener(subscription);
+    };
+  }, [data]);
+
+  if (!chart) return null;
+
+  return (
+    <Path
+      d={chart.curves[index].sector.path.print()}
+      fill={data[index].color}
+    />
+  );
+}
